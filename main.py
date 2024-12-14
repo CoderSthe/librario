f18d3505dbbff7c73fb8855ba9ddf5bb32e9defb
Sithembiso Mdhluli <mdhluli269@gmail.com>
2024-12-14 14:30:35 +0200

    update app.config

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import abort, Api, fields, marshal_with, Resource, reqparse
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 api = Api(app)
 
