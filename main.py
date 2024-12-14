30dda5b290913fc69bd4a2b50ee3b5208e3c9bc5
Sithembiso Mdhluli <mdhluli269@gmail.com>
2024-12-14 13:48:34 +0200

    add book arguments and book fields

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import (
-    abort,
-    Api,
-    fields,
-    marshal_with,
-    Resource,
-    reqparse
-)
+from flask_restful import abort, Api, fields, marshal_with, Resource, reqparse
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite///database.db"
 db = SQLAlchemy(app)
 api = Api(app)
+
 
 class Librario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,3 +17,20 @@ class Librario(db.Model):
 
     def __repr__(self):
         return f"Name: {self.name}, Author: {self.author}, Year Released: {self.year}, ISBN Number: {self.isbn}"
+
+
+book_args = reqparse.RequestParser()
+book_args.add_argument("name", type=str, required=True, help="name cannot be empty.")
+book_args.add_argument(
+    "author", type=str, required=True, help="author cannot be empty."
+)
+book_args.add_argument("year", type=int, required=True, help="year cannot be empty.")
+book_args.add_argument("isbn", type=int, required=True, help="isbn cannot be empty.")
+
+book_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "author": fields.String,
+    "year": fields.Integer,
+    "isbn": fields.Integer
+}
