0ead8e3be599cf50d5dacef6347f54d0f5fd66e3
Sithembiso Mdhluli <mdhluli269@gmail.com>
2024-12-14 12:53:21 +0200

    Revert "change class name"
    This reverts commit a9d8d38d19d65b1263dac8d1f153cb2817e7cc7a.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///database.db'
 db = SQLAlchemy(app)
 api = Api(app)
 
-class Books(db.Model):
+class Librario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     author = db.Column(db.String(80), unique=False, nullable=False)
@@ -23,10 +23,3 @@ class Books(db.Model):
 
     def __repr__(self):
         return f"Name: {self.name}, Author: {self.author}, Year Released: {self.year}, ISBN Number: {self.isbn}"
-
-@app.route('/')
-def home():
-    return "<h1>Welcome to Librario!</h1>"
-
-if __name__ == "__main__":
-    app.run(debug=True)
